171888ca07bc38a1618f9d5588f7ffc7d9a7f147
anandclicks <puspanand94@gmail.com>
2024-12-14 18:36:54 +0530

    cors policy allow for every domain

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({extended : true}))
 app.use('/images',express.static(path.join(__dirname,'images')))
 app.use(cors({
-  origin : '*',
+  origin :'*'
 }))
 
 // Importing Routes handler 
